7fafeb378947f8feb21a8e3ba64bb5698bd0a97e
Thoai1503 <145348246+Thoai1503@users.noreply.github.com>
2025-06-29 10:14:34 +0700

    Initial commit - upload ASP.NET MVC project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Insurance_Agency_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Services()
+        {
+            Session["display"] = 0;
+            ViewBag.Message = "Your services page.";
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            ViewBag.Message = "Your privacy policy page.";
+            return View();
+        }
+        public ActionResult Feature()
+        {
+            ViewBag.Message = "Your privacy policy page.";
+            return View();
+        }
+        public ActionResult Team()
+        {
+            ViewBag.Message = "Your privacy policy page.";
+            return View();
+        }
+        public ActionResult Testimonial()
+        {
+            ViewBag.Message = "Your privacy policy page.";
+            return View();
+        }
+        public ActionResult DemoForm()
+        {
+            ViewBag.Message = "Your privacy policy page.";
+            return View();
+        }
+        public ActionResult TextEditor()
+        {
+            ViewBag.Message = "Your text editor page.";
+            return View();
+        }
+    }
+}
